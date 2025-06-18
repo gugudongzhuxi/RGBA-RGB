eb5e14b817d85d0f6471457ca6f4b7adaddaba02
谷谷董 <148513293+gugudongzhuxi@users.noreply.github.com>
2025-06-18 17:59:11 +0800

    Update 叠加.py

--- 叠加.py
+++ 叠加.py
@@ -133,8 +133,8 @@ def overlay_images(material_dir, target_dir, output_dir):
 
 # 主函数
 if __name__ == "__main__":
-    material_dir = "/data_4T/zy_GCJX_607/code/PS/素材"
-    target_dir = "/data_4T/zy_GCJX_607/code/PS/yolo测试_无标签"
-    output_dir = "/data_4T/zy_GCJX_607/code/PS/叠加结果_1"
+    material_dir = "/data_4T//code/PS/素材"
+    target_dir = "/data_4T//code/PS/yolo测试_无标签"
+    output_dir = "/data_4T//code/PS/叠加结果_1"
     
     overlay_images(material_dir, target_dir, output_dir)
